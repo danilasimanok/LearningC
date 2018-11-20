68f9a819e895b920d843fad77eb02c85bdf44eb8
danilasimanok <dsimanok@bk.ru>
2018-11-20 14:49:51 +0300

    Добавлен алгоритм Уоршелла.

---
+++ warshall/warshall.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+#define n 5
+
+void printr(int R[n][n])
+{
+	int i,j;
+	for(i=n-1;i>=0;i--){
+		for(j=0;j<n;j++){
+			printf("%i ",R[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int R[n][n]={{0,0,0,0,0},{1,0,0,0,0},{1,1,0,0,0},{0,0,1,0,0},{0,0,1,1,0}};
+	printr(R);
+	
+	int i,j,k;
+	int T[n][n];
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			T[i][j]=R[i][j];
+	for(k=0;k<n;k++)
+		for(i=0;i<n;i++)
+			for(j=0;j<n;j++)
+				T[i][j]=T[i][j]||(T[i][k]&&T[k][j]);
+	
+	printf("\n");
+	printr(T);
+	return 0;
+}
